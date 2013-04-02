1a20586523ab88168587dcdf9531c8b1431c70c8
Jens Ådne Rydland <jensadne@geekevents.org>
2013-04-02 14:01:34 +0200

    migrations that work for us. (fuckings to bugs)

--- src/reversion/migrations/0001_initial.py
+++ src/reversion/migrations/0001_initial.py
@@ -1,103 +1,159 @@
-# encoding: utf-8
+# -*- coding: utf-8 -*-
 import datetime
 from south.db import db
 from south.v2 import SchemaMigration
 from django.db import models
 
-try:
-    from django.contrib.auth import get_user_model
-except ImportError: # django < 1.5
-    from django.contrib.auth.models import User
-else:
-    User = get_user_model()
-
 
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        
         # Adding model 'Revision'
-        db.create_table('reversion_revision', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        db.create_table(u'reversion_revision', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('manager_slug', self.gf('django.db.models.fields.CharField')(default=u'default', max_length=200, db_index=True)),
             ('date_created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
-            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm["%s.%s" % (User._meta.app_label, User._meta.object_name)], null=True, blank=True)),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['hsauth.User'], null=True, blank=True)),
             ('comment', self.gf('django.db.models.fields.TextField')(blank=True)),
         ))
-        db.send_create_signal('reversion', ['Revision'])
+        db.send_create_signal(u'reversion', ['Revision'])
 
         # Adding model 'Version'
-        db.create_table('reversion_version', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        db.create_table(u'reversion_version', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('revision', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['reversion.Revision'])),
             ('object_id', self.gf('django.db.models.fields.TextField')()),
+            ('object_id_int', self.gf('django.db.models.fields.IntegerField')(db_index=True, null=True, blank=True)),
             ('content_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['contenttypes.ContentType'])),
             ('format', self.gf('django.db.models.fields.CharField')(max_length=255)),
             ('serialized_data', self.gf('django.db.models.fields.TextField')()),
             ('object_repr', self.gf('django.db.models.fields.TextField')()),
+            ('type', self.gf('django.db.models.fields.PositiveSmallIntegerField')(db_index=True)),
         ))
-        db.send_create_signal('reversion', ['Version'])
+        db.send_create_signal(u'reversion', ['Version'])
+
 
     def backwards(self, orm):
-        
         # Deleting model 'Revision'
-        db.delete_table('reversion_revision')
+        db.delete_table(u'reversion_revision')
 
         # Deleting model 'Version'
-        db.delete_table('reversion_version')
+        db.delete_table(u'reversion_version')
+
 
     models = {
-        'auth.group': {
-            'Meta': {'object_name': 'Group'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
-        },
-        'auth.permission': {
-            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
-            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        },
-        "%s.%s" % (User._meta.app_label, User._meta.module_name): {
-            'Meta': {'object_name': User.__name__ },
-            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+        u'actor.actor': {
+            'Meta': {'object_name': 'Actor', 'db_table': "u'actor'"},
+            'actor_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['actor.ActorType']"}),
+            'addresses': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['locations.Location']", 'symmetrical': 'False'}),
+            'bitflag': ('core.models.fields.HSBitflagField', [], {'default': '0', 'bitflagmask': '39'}),
+            'contactable': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'db_index': 'True'}),
             'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
-            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'flag': ('django.db.models.fields.CharField', [], {'default': "u'a'", 'max_length': '1', 'db_index': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
             'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
-            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+            'phone': ('django.db.models.fields.CharField', [], {'max_length': '16', 'blank': 'True'}),
+            'phone2': ('django.db.models.fields.CharField', [], {'max_length': '16', 'blank': 'True'})
         },
-        'contenttypes.contenttype': {
+        u'actor.actortype': {
+            'Meta': {'object_name': 'ActorType', 'db_table': "u'actor_type'"},
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'db_index': 'True'}),
+            'flag': ('django.db.models.fields.CharField', [], {'default': "u'a'", 'max_length': '1', 'db_index': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'subject': ('django.db.models.fields.CharField', [], {'max_length': '64'})
+        },
+        u'contenttypes.contenttype': {
             'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
             'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
-        'reversion.revision': {
+        u'hsauth.user': {
+            'Meta': {'object_name': 'User', '_ormbases': [u'actor.Actor']},
+            u'actor_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['actor.Actor']", 'unique': 'True', 'primary_key': 'True'}),
+            'birth': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'gender': ('django.db.models.fields.CharField', [], {'default': "u'u'", 'max_length': '1'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'social_security_number': ('core.models.fields.EncryptedCharField', [], {'max_length': '165', 'null': 'True', 'blank': 'True'})
+        },
+        u'locations.country': {
+            'Meta': {'object_name': 'Country', 'db_table': "u'country'"},
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'db_index': 'True'}),
+            'flag': ('django.db.models.fields.CharField', [], {'default': "u'a'", 'max_length': '1', 'db_index': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'iso': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
+            'iso3': ('django.db.models.fields.CharField', [], {'max_length': '3'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'phone_number_regex': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'phone_prefix': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'postal_code_regex': ('django.db.models.fields.CharField', [], {'max_length': '16'}),
+            'second_level_name': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'third_level_name': ('django.db.models.fields.CharField', [], {'max_length': '32'})
+        },
+        u'locations.county': {
+            'Meta': {'ordering': "(u'country', u'identifier')", 'object_name': 'County', 'db_table': "u'county'"},
+            'country': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'counties'", 'to': u"orm['locations.Country']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'db_index': 'True'}),
+            'flag': ('django.db.models.fields.CharField', [], {'default': "u'a'", 'max_length': '1', 'db_index': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'identifier': ('django.db.models.fields.CharField', [], {'max_length': '8'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '64'})
+        },
+        u'locations.location': {
+            'Meta': {'object_name': 'Location', 'db_table': "u'location'"},
+            'address1': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'address2': ('django.db.models.fields.CharField', [], {'max_length': '128', 'blank': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'db_index': 'True'}),
+            'flag': ('django.db.models.fields.CharField', [], {'default': "u'a'", 'max_length': '1', 'db_index': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '128', 'blank': 'True'}),
+            'postal_code': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['locations.PostalCode']"}),
+            'subject': ('django.db.models.fields.CharField', [], {'max_length': '16'})
+        },
+        u'locations.municipality': {
+            'Meta': {'object_name': 'Municipality', 'db_table': "u'municipality'"},
+            'county': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'municipalities'", 'to': u"orm['locations.County']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'db_index': 'True'}),
+            'flag': ('django.db.models.fields.CharField', [], {'default': "u'a'", 'max_length': '1', 'db_index': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'identifier': ('django.db.models.fields.CharField', [], {'max_length': '8'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '64'})
+        },
+        u'locations.postalcode': {
+            'Meta': {'object_name': 'PostalCode', 'db_table': "u'postal_code'"},
+            'address_type': ('django.db.models.fields.PositiveIntegerField', [], {'default': '1'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'db_index': 'True'}),
+            'flag': ('django.db.models.fields.CharField', [], {'default': "u'a'", 'max_length': '1', 'db_index': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'municipality': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'postal_codes'", 'to': u"orm['locations.Municipality']"}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'zip': ('django.db.models.fields.CharField', [], {'max_length': '16'})
+        },
+        u'reversion.revision': {
             'Meta': {'object_name': 'Revision'},
             'comment': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
             'date_created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['%s.%s']"% (User._meta.app_label, User._meta.object_name), 'null': 'True', 'blank': 'True'})
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'manager_slug': ('django.db.models.fields.CharField', [], {'default': "u'default'", 'max_length': '200', 'db_index': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['hsauth.User']", 'null': 'True', 'blank': 'True'})
         },
-        'reversion.version': {
+        u'reversion.version': {
             'Meta': {'object_name': 'Version'},
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
             'format': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'object_id': ('django.db.models.fields.TextField', [], {}),
+            'object_id_int': ('django.db.models.fields.IntegerField', [], {'db_index': 'True', 'null': 'True', 'blank': 'True'}),
             'object_repr': ('django.db.models.fields.TextField', [], {}),
-            'revision': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['reversion.Revision']"}),
-            'serialized_data': ('django.db.models.fields.TextField', [], {})
+            'revision': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['reversion.Revision']"}),
+            'serialized_data': ('django.db.models.fields.TextField', [], {}),
+            'type': ('django.db.models.fields.PositiveSmallIntegerField', [], {'db_index': 'True'})
         }
     }
 
